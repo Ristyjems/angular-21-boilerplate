64c5f8272640e74f2cbd54cdba71cc6236a77283
risty112005 <Ristyjems>
2026-05-08 14:58:15 +0800

    Updated Frontend

--- ipt-2026-frontend/src/app/profile/details.component.ts
+++ ipt-2026-frontend/src/app/profile/details.component.ts
@@ -1,10 +1,12 @@
-﻿import { Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { AccountService } from '@app/_services';
 
-@Component({ templateUrl: 'details.component.html' })
+@Component({ templateUrl: 'details.component.html', standalone: false })
 export class DetailsComponent {
-    account = this.accountService.accountValue;
-
     constructor(private accountService: AccountService) { }
+
+    get account() {
+        return this.accountService.accountValue;
+    }
 }
