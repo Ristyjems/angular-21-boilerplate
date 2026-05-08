85725744dc858abc265a10de1992e7fb14f40b40
risty112005 <Ristyjems>
2026-05-08 14:33:56 +0800

    Updated Frontend

--- ipt-2026-frontend/src/app/home/home.component.ts
+++ ipt-2026-frontend/src/app/home/home.component.ts
@@ -1,10 +1,12 @@
-﻿import { Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { AccountService } from '@app/_services';
 
-@Component({ templateUrl: 'home.component.html' })
+@Component({ templateUrl: 'home.component.html', standalone: false })
 export class HomeComponent {
-    account = this.accountService.accountValue;
-
     constructor(private accountService: AccountService) { }
+
+    get account() {
+        return this.accountService.accountValue;
+    }
 }
